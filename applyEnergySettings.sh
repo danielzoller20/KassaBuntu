63d882414a000446f14c54a673c4fdbeb5fe6c22
danielzoller20 <danielzoller20@gmail.com>
2024-12-24 09:22:52 +0100

    ignore closing of lid

--- applyEnergySettings.sh
+++ applyEnergySettings.sh
@@ -10,3 +10,12 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 
 # Disable power saving options for display on battery and AC
 gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+
+
+## ignore closing lid
+
+# Define the file to modify
+CONFIG_FILE="/etc/systemd/logind.conf"
+
+# Use sed to uncomment and change the HandleLidSwitch value
+sudo sed -i '/^#HandleLidSwitch=/s/^#//; s/HandleLidSwitch=.*/HandleLidSwitch=ignore/' "$CONFIG_FILE"
